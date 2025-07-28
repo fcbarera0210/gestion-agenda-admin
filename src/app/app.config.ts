e2f0458c59ce638c74f18afb1ddd290fa082edd4
fcabrera0210 <fcabrera0210@gmail.com>
2025-07-28 19:16:01 -0400

    🔥Initial commit: Project setup with Angular and Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "gestion-agenda-pro", appId: "1:135115227347:web:85880d4f669d90359f5426", storageBucket: "gestion-agenda-pro.firebasestorage.app", apiKey: "AIzaSyCrVIIKMjAOhcGqihB7lRrB9T9TVolPR0c", authDomain: "gestion-agenda-pro.firebaseapp.com", messagingSenderId: "135115227347" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions())
   ]
 };
